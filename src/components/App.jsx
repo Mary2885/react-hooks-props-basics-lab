cc50e5130e39b2435078cea5484761ab9810dfae
mary <Mariashee001@gmail.com>
2024-05-02 12:07:42 +0300

    complete

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { Router, Route } from "react-router-dom";
+
+import Home from ".."
+import Contact from ".."
+
+
+const App = () => {
+    return (
+        <>
+        <NavBar />
+        <Router>
+            <Route path="/" elements
+        </Router>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import reportWebVitals from "../reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+        <App></App>
+        
+        </BrowserRouter>
+    </React.StrictMode>
+)
 
 ReactDOM.render(<App />, document.getElementById("root"));
